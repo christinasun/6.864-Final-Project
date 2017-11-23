31991c9d7da5b1d018dfb3348040ac21f94545fa
Tally <tportnoi@mit.edu>
2017-11-23 00:14:17 -0600

    created abstract ask ubuntu model

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -21,11 +21,10 @@ def get_model(embeddings, args):
         raise Exception("Model name {} not supported!".format(args.model_name))
 
 
-
-class CNN(nn.Module):
+class AbstractAskUbuntuModel(nn.Module):
 
     def __init__(self, embeddings, args):
-        super(CNN, self).__init__()
+        super(AbstractAskUbuntuModel, self).__init__()
 
         self.args = args
         vocab_size, embed_dim = embeddings.shape
@@ -35,8 +34,7 @@ class CNN(nn.Module):
 
         self.W_hidden = nn.Linear(embed_dim, 200)
         self.W_out = nn.Linear(200, 200)
-        pass
-
+        return
 
     def forward(self, q_title_tensors, q_body_tensors, candidate_title_tensors, candidate_body_tensors):
         q_title_embeddings = self.forward_helper(q_title_tensors)
@@ -74,22 +72,29 @@ class CNN(nn.Module):
         return output
 
     def forward_helper(self, tensor):
+        pass
+
+
+class CNN(AbstractAskUbuntuModel):
+
+    def __init__(self, embeddings, args):
+        super(CNN, self).__init__(embeddings, args)
+
+    def forward_helper(self, tensor):
         all_x = self.embedding_layer(tensor)
         avg_x = torch.mean(all_x, dim=1)
         hidden = F.relu( self.W_hidden(avg_x) )
         out = self.W_out(hidden)
         return out
-
-    def cosine_similarity(self, q_embeddings, candidate_embeddings):
-        pass
         
 
 
-class LSTM(nn.Module):
-    # TODO: Implement
+class LSTM(AbstractAskUbuntuModel):
 
-    def __init__(self):
-        pass
+
+    def __init__(self, embeddings, args):
+        super(LSTM, self).__init__(embeddings, args)
 
     def forward(self):
+        # TODO: Implement
         pass
