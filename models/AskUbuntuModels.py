3116cb62f13cfc490104c577b1a97dcfb732d85a
Tally <tportnoi@mit.edu>
2017-12-07 16:32:44 -0500

    changed BOW model to be identity

--- models/AskUbuntuModels.py
+++ models/AskUbuntuModels.py
@@ -12,7 +12,6 @@ class AbstractAskUbuntuModel(nn.Module):
 
         self.args = args
 
-
         vocab_size, embed_dim = embeddings.shape
 
         self.embedding_layer = nn.Embedding(vocab_size, embed_dim)
@@ -28,44 +27,33 @@ class AbstractAskUbuntuModel(nn.Module):
                 q_body_tensors,
                 candidate_title_tensors,
                 candidate_body_tensors):
-        q_title_embeddings = self.forward_helper(q_title_tensors)
-        # if self.args.debug: misc_utils.print_shape_variable('q_title_embeddings', q_title_embeddings)
-        q_body_embeddings = self.forward_helper(q_body_tensors)
-        # if self.args.debug: misc_utils.print_shape_variable('q_body_embeddings', q_body_embeddings)
-        q_output_before_mean = torch.stack([q_title_embeddings, q_body_embeddings])
-        # if self.args.debug: misc_utils.print_shape_variable('q_output_before_mean', q_output_before_mean)
-        q_output = torch.mean(q_output_before_mean, dim=0)
-        # if self.args.debug: misc_utils.print_shape_variable('q_output', q_output)
 
-        num_candidates, d2, d3 = candidate_title_tensors.size()
+        q_title_encodings = self.forward_helper(q_title_tensors)
+        q_body_encodings = self.forward_helper(q_body_tensors)
+        q_encoding_before_mean = torch.stack([q_title_encodings, q_body_encodings])
+        q_encoding = torch.mean(q_encoding_before_mean, dim=0)
 
-        title_embeddings = self.forward_helper(candidate_title_tensors.view(num_candidates*d2,d3))
-        # if self.args.debug: misc_utils.print_shape_variable('title_embeddings', title_embeddings)
-        body_embeddings = self.forward_helper(candidate_body_tensors.view(num_candidates*d2,d3))
-        # if self.args.debug: misc_utils.print_shape_variable('body_embeddings', body_embeddings)
-        candidate_outputs_before_mean = torch.stack([title_embeddings, body_embeddings])
-        # if self.args.debug: misc_utils.print_shape_variable('candidate_outputs_before_mean', candidate_outputs_before_mean)
-        candidate_outputs = torch.mean(candidate_outputs_before_mean, dim=0)
-        # if self.args.debug: misc_utils.print_shape_variable('candidate_outputs', candidate_outputs)
+        num_candidates, batch_size, embedding_dim = candidate_title_tensors.size()
 
+        # get the encodings for the flattened out candidate tensors
+        title_encodings = self.forward_helper(candidate_title_tensors.view(num_candidates*batch_size,embedding_dim))
+        body_encodings = self.forward_helper(candidate_body_tensors.view(num_candidates*batch_size,embedding_dim))
+        candidate_encodings_before_mean = torch.stack([title_encodings, body_encodings])
+        candidate_encodings = torch.mean(candidate_encodings_before_mean, dim=0)
 
-        expanded_q_output0 = q_output.view(1,d2,self.hidden_dim)
-        # if self.args.debug: misc_utils.print_shape_variable('expanded_q_output0', expanded_q_output0)
-        expanded_q_output1 = expanded_q_output0.expand(num_candidates,d2,self.hidden_dim)
-        # if self.args.debug: misc_utils.print_shape_variable('expanded_q_output1', expanded_q_output1)
-        expanded_q_output2 = expanded_q_output1.contiguous().view(num_candidates*d2,self.hidden_dim)
-        # if self.args.debug: misc_utils.print_shape_variable('expanded_q_output2', expanded_q_output2)
+        # expand and flatten q encodings so that we can pass it through cosine similarities with flattened out
+        # candidate encodings
+        expanded_q_encoding = q_encoding.view(1,batch_size,self.hidden_dim)
+        expanded_q_encoding = expanded_q_encoding.expand(num_candidates,batch_size,self.hidden_dim)
+        expanded_q_encoding = expanded_q_encoding.contiguous().view(num_candidates*batch_size,self.hidden_dim)
 
-        expanded_cosine_similarities = F.cosine_similarity(expanded_q_output2, candidate_outputs, dim=1)
-        # if self.args.debug: misc_utils.print_shape_variable('expanded_cosine_similarities', expanded_cosine_similarities)
+        # compute cosine similarities
+        expanded_cosine_similarities = F.cosine_similarity(expanded_q_encoding, candidate_encodings, dim=1)
 
-        # TODO: Double check that this transformation is resizing the output as desired
-        output0 = expanded_cosine_similarities.view(num_candidates,d2,1)
-        # if self.args.debug: misc_utils.print_shape_variable('output0', output0)
-        output1 = output0.view(num_candidates,d2).t() # .t() is transpose
-        # if self.args.debug: misc_utils.print_shape_variable('output1', output1)
+        # un-flatten cosine similarities so that we get output of size batch_size * num_candidates (t() is transpose)
+        output = expanded_cosine_similarities.view(num_candidates,batch_size,1).view(num_candidates,batch_size).t()
 
-        return output1
+        return output
 
     def forward_helper(self, tensor):
         pass
@@ -93,7 +81,8 @@ class CNN(AbstractAskUbuntuModel):
     def __init__(self, embeddings, args):
         super(CNN, self).__init__(embeddings, args)
         vocab_size, embed_dim = embeddings.shape
-        self.conv = nn.Conv1d(embed_dim, self.hidden_dim, 3, padding=2)
+        # TODO: make kernel size a parameter
+        self.conv = nn.Conv1d(embed_dim, self.hidden_dim, 3, padding=(2,0))
         self.tanh = nn.Tanh()
         self.dropout = nn.Dropout(p=self.args.dropout)
         self.pooling = 'mean'
@@ -109,7 +98,7 @@ class CNN(AbstractAskUbuntuModel):
         if self.pooling == 'mean':
             lengths = torch.sum(mask,1)
             lengths = torch.unsqueeze(lengths,1)
-            lengths = lengths.expand(tensor.data.shape)
+            lengths = lengths.expand(mask.data.shape)
             mask = torch.div(mask,lengths)
 
         x = self.embedding_layer(tensor) # (batch size, width (length of text), height (embedding dim))
@@ -117,13 +106,12 @@ class CNN(AbstractAskUbuntuModel):
         hiddens = self.conv(x_perm)
         post_dropout = self.dropout(hiddens)
         tanh_x = self.tanh(post_dropout)
-        tanh_x_cropped = tanh_x[:,:,:self.args.len_query]
 
-        N, hd, co =  tanh_x_cropped.data.shape
+        N, hd, co =  tanh_x.data.shape
         mask = torch.unsqueeze(mask,1)
         expanded_mask = mask.expand(N, hd, co)
 
-        masked = torch.mul(expanded_mask,tanh_x_cropped)
+        masked = torch.mul(expanded_mask,tanh_x)
 
         if self.pooling == 'mean':
             summed = torch.sum(masked, dim=2)
@@ -188,26 +176,7 @@ class BOW(AbstractAskUbuntuModel):
         super(BOW, self).__init__(embeddings, args)
         self.vocab_size, embed_dim = embeddings.shape
         self.hidden_dim = self.vocab_size
-        self.name = 'bow'
+        self.name = 'identity'
 
     def forward_helper(self, tensor):
-        batch_size, seq_len = tensor.data.shape
-        out = torch.Tensor(batch_size,self.vocab_size)
-        df = torch.Tensor(1,self.vocab_size)
-        for i in range(batch_size):
-            sample = tensor[i].unsqueeze(0)
-            mask = (sample != 0)
-            length = torch.sum(mask,1).data[0]
-            indices = sample.narrow(1,0,length)
-            for j in range(length):
-                df[0][indices[0][j].data[0]] += 1
-        for i in range(batch_size):
-            sample = tensor[i].unsqueeze(0)
-            mask = (sample != 0)
-            length = torch.sum(mask,1).data[0]
-            indices = sample.narrow(1,0,length)
-            for j in range(length):
-                # out[i][indices[0][j].data[0]] += 1*(1+np.log((1+batch_size)/(1+df)))
-                index = indices[0][j].data[0]
-                out[i][index] += 1*(1+np.log((1+batch_size)/(1+df[0][index])))
-        return autograd.Variable(out)
+        return tensor
