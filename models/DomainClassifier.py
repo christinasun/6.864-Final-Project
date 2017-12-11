5e42759d0a024fecbf89e40f162c37e1ca2bb59d
Tally <tportnoi@mit.edu>
2017-12-11 17:21:12 -0500

    added non-linearity to domain classifier

--- models/DomainClassifier.py
+++ models/DomainClassifier.py
@@ -15,14 +15,17 @@ class DomainClassifier(nn.Module):
         self.hidden_2_dim = 150
 
         self.hidden_1 = nn.Linear(self.input_dim, self.hidden_1_dim)
+        self.relu_1 = nn.ReLU()
         self.hidden_2 = nn.Linear(self.hidden_1_dim, self.hidden_2_dim)
+        self.relu_2 = nn.ReLU()
         self.hidden_3 = nn.Linear(self.hidden_2_dim, 1)
-        self.softmax = nn.Softmax()
-
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, encoding):
-        h1 = self.hidden_1(encoding)
-        h2 = self.hidden_2(h1)
+        z1 = self.hidden_1(encoding)
+        h1 = self.relu_1(z1)
+        z2 = self.hidden_2(h1)
+        h2 = self.relu_2(z2)
         h3 = self.hidden_3(h2)
-        out = self.softmax(h3)
+        out = self.sigmoid(h3)
         return out
