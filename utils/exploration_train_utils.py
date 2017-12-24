12e3a7d6846975e4b64c6576972bcb9cea266794
Tally <tportnoi@mit.edu>
2017-12-23 23:43:33 -0600

    removed print

--- utils/exploration_train_utils.py
+++ utils/exploration_train_utils.py
@@ -128,8 +128,6 @@ def run_epoch(label_predictor_train_data, adversary_train_data_generator, is_tra
 
         cosine_similarities, reconstruction_loss = label_predictor(q_title_tensors, q_body_tensors,
                                     selected_candidate_title_tensors, selected_candidate_body_tensors)
-        print "reconstruction loss"
-        print reconstruction_loss
         domain_labels = adversary(title_tensors, body_tensors)
 
         # if args.debug: misc_utils.print_shape_variable('cosine_similarities', cosine_similarities)
